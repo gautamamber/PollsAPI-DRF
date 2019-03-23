f2780348c8439f8ef5416ee8a115ecded3ad15a2
gautamamber <ambergautam1@gmail.com>
2019-03-23 11:09:41 +0530

    Implement API in pure DJANGO, without using restframework

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Poll, Choice, Vote
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+# Pure django API endpoints without rest framework
+
+def polls_list(request):
+	polls = Poll.objects.all()[:20]
+	data = {"results": list(polls.values("question", "created_by__username", "pub_date"))}
+	return JsonResponse(data)
+
+def polls_detail(request, pk):
+	poll = get_object_or_404(Poll, pk = pk)
+	data = {"results": {"question": poll.question,"created_by": poll.created_by.username,"pub_date": poll.pub_date}}
+	return JsonResponse(data)
+
+# check with 
+# "curl http://localhost:8000/polls"
+# "curl http://localhost:8000/polls/1"
+
